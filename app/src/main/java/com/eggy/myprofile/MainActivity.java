8b2b858c217bdd376bb515350015e7734d877a00
eggywardani <eggyandikawardani@gmail.com>
2020-10-12 21:47:14 +0700

    Initial commit

---
+++ app/src/main/java/com/eggy/myprofile/MainActivity.java
@@ -0,0 +1,37 @@
+package com.eggy.myprofile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.about){
+            startActivity(new Intent(this, AboutActivity.class));
+        }else if (item.getItemId() == R.id.setting){
+            startActivity(new Intent(this, SettingActivity.class));
+        }else if (item.getItemId() == R.id.help){
+            startActivity(new Intent(this, HelpActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
